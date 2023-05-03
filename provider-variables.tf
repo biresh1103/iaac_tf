3b699d11a76421990454257847d081f01337777d
biresh1103 <39379039+biresh1103@users.noreply.github.com>
2023-05-03 12:23:59 -0600

    variable for tf

---
+++ provider-variables.tf
@@ -0,0 +1,14 @@
+variable "aws-access-key"{
+ type = string
+ description = "AWS access Key"
+}
+
+variable "aws-secret-key" {
+ type = string
+ description = "AWS secret key"
+}
+
+variable "aws-region"{
+ type = string
+ description = "AWS region"
+}
