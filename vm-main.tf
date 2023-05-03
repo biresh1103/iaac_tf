f360ac413c58ea74b929ed0bcddbb915d0872d07
biresh1103 <39379039+biresh1103@users.noreply.github.com>
2023-05-03 12:40:08 -0600

    Create vm-main.tf

---
+++ vm-main.tf
@@ -0,0 +1,36 @@
+# Define the security group for the EC2 Instance
+resource "aws_security_group" "aws-vm-sg" {
+  name        = "vm-sg"
+  description = "Allow incoming connections"
+  vpc_id      = aws_vpc.vpc.id  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming RDP connections (Windows)"
+  }  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections (Linux)"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  tags = {
+    Name = "windows-sg"
+  }
+}
