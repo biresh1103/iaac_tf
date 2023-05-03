a55abc3ff89d955a98755c22ffc5353765b4807a
biresh1103 <39379039+biresh1103@users.noreply.github.com>
2023-05-03 12:54:04 -0600

    Update vm-main.tf

--- vm-main.tf
+++ vm-main.tf
@@ -1,3 +1,36 @@
+# Create EC2 Instance
+resource "aws_instance" "vm-server" {
+  ami                    = data.aws_ami.rhel_8_5.id
+  instance_type          = var.vm_instance_type
+  subnet_id              = aws_subnet.public-subnet.id
+  vpc_security_group_ids = [aws_security_group.aws-vm-sg.id]
+  source_dest_check      = false
+  key_name               = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = var.vm_associate_public_ip_address
+  
+  user_data = file("aws-user-data.sh")
+  
+  # root disk
+  root_block_device {
+    volume_size           = var.vm_root_volume_size
+    volume_type           = var.vm_root_volume_type
+    delete_on_termination = true
+    encrypted             = true
+  }
+  # extra disk
+  ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_size           = var.vm_data_volume_size
+    volume_type           = var.vm_data_volume_type
+    encrypted             = true
+    delete_on_termination = true
+  }
+  
+  tags = {
+    Name = "linux-server-vm"
+  }
+}
+
 # Define the security group for the EC2 Instance
 resource "aws_security_group" "aws-vm-sg" {
   name        = "vm-sg"
